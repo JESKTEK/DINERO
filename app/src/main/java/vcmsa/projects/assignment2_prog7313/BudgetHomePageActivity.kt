ade7e937ead27fa8c0f4c7b960d933af6149696b
JadinNaicker <st10275486@vcconnect.edu.za>
2025-04-29 18:29:07 +0200

    Did the backend and some front end functionality for the add category page

--- app/src/main/java/vcmsa/projects/assignment2_prog7313/BudgetHomePageActivity.kt
+++ app/src/main/java/vcmsa/projects/assignment2_prog7313/BudgetHomePageActivity.kt
@@ -1,20 +1,29 @@
 package vcmsa.projects.assignment2_prog7313
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+import androidx.cardview.widget.CardView
+
+
 class BudgetHomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_budget_home_page)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val createCategoryCard = findViewById<CardView>(R.id.cardCreateCategory)
+        createCategoryCard.setOnClickListener {
+            val intent = Intent(this, CategoryActivity::class.java)
+            startActivity(intent)
         }
     }
 }
