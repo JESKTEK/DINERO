91b19346f8b90a47a11243aff42a0720c1e9395f
ST10252440 Kian Jose Lima Campbell <st10252440@vcconnect.edu.za>
2025-04-29 11:49:46 +0200

    Created Rudimentary Category Page

---
+++ app/src/main/java/vcmsa/projects/assignment2_prog7313/CategoryAdapter.kt
@@ -0,0 +1,61 @@
+package vcmsa.projects.assignment2_prog7313
+
+
+import android.graphics.BitmapFactory
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.ImageView
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+import vcmsa.projects.assignment2_prog7313.R
+import vcmsa.projects.assignment2_prog7313.databinding.RecyclerCategoryLayoutBinding
+import java.io.ByteArrayInputStream
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
+
+class CategoryAdapter (private var catList: List<Category>): RecyclerView.Adapter<CategoryAdapter.categoryViewHolder>() {
+
+    private val firestore = FirebaseFirestore.getInstance()
+    class categoryViewHolder(val binding: RecyclerCategoryLayoutBinding): RecyclerView.ViewHolder(binding.root)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): categoryViewHolder {
+        val binding = RecyclerCategoryLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return categoryViewHolder(binding)
+        //val view = LayoutInflater.from(parent.context)
+        //    .inflate(R.layout.recycler_item_layout, parent, false)
+        //return UserViewHolder(view)
+    }
+
+    @OptIn(ExperimentalEncodingApi::class)
+    override fun onBindViewHolder(holder: categoryViewHolder, position: Int) {
+        val category = catList[position]
+
+        holder.binding.tvAddExpenseButton.setOnClickListener {
+            onAddExpenseClick(category, position, holder)
+        }
+
+        //holder.binding.tvProfileImage.setImageResource(post.uploadImage)
+        holder.binding.tvName.text = category.catName
+        holder.binding.tvDate.text = category.dateCreated
+        holder.binding.tvAmtSpent.text = category.amountSpent.toString()
+        holder.binding.tvAmtBudget.text = category.amountBudgeted.toString()
+    }
+
+    override fun getItemCount() = catList.size
+
+    public fun onAddExpenseClick(category: Category, position: Int, holder: categoryViewHolder) {
+        //val postChoice = postList[position]
+        //postChoice.likes++
+        return
+    }
+
+    fun updateData(newList: List<Category>) {
+        catList = newList
+        notifyDataSetChanged()
+    }
+}
