133253e6404bc6f023663d9fff557b8c7b3e48b7
ST10252440 Kian Jose Lima Campbell <st10252440@vcconnect.edu.za>
2025-05-01 19:56:48 +0200

    Fixed Date Selectors in View Category Page

--- app/src/main/java/vcmsa/projects/assignment2_prog7313/CategoryView.kt
+++ app/src/main/java/vcmsa/projects/assignment2_prog7313/CategoryView.kt
@@ -1,6 +1,8 @@
 package vcmsa.projects.assignment2_prog7313
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -10,11 +12,17 @@ import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.launch
 import vcmsa.projects.assignment2_prog7313.databinding.ActivityCategoryViewBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
 
 class CategoryView : AppCompatActivity() {
 
     private lateinit var binding: ActivityCategoryViewBinding
     private lateinit var categoryAdapter: CategoryAdapter
+
+    private var unfilteredCategories: List<Category> = emptyList()
+    private var filteredCategories: List<Category> = emptyList()
 
     //private lateinit var database: AppDatabase
     private val firestore = Firebase.firestore
@@ -37,6 +45,50 @@ class CategoryView : AppCompatActivity() {
             loadCategories()
         }
 
+
+
+        var datesInput1 = false
+        var datesInput2 = false
+        binding.fromDateSel.setOnClickListener() {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePicker = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+                binding.fromDateSel.setText("$selectedDay/${selectedMonth + 1}/$selectedYear")
+                if (datesInput1 && datesInput2) {
+                    filterCategoriesDate(
+                        binding.fromDateSel.text.toString(),
+                        binding.toDateSel.text.toString()
+                    )
+                }
+            }, year, month, day)
+
+            datePicker.show()
+            datesInput1 = true
+
+        }
+        binding.toDateSel.setOnClickListener() {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePicker = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+                binding.toDateSel.setText("$selectedDay/${selectedMonth + 1}/$selectedYear")
+                datesInput2 = true
+                if (datesInput1 && datesInput2) {
+                    filterCategoriesDate(
+                        binding.fromDateSel.text.toString(),
+                        binding.toDateSel.text.toString()
+                    )
+                }
+            }, year, month, day)
+
+            datePicker.show()
+
+        }
 
         //enableEdgeToEdge()
         /*setContentView(R.layout.activity_category_view)
@@ -82,6 +134,31 @@ class CategoryView : AppCompatActivity() {
         }
 
     }
+    private fun filterCategoriesDate(fromDateS: String, toDateS: String){
+
+
+        val fromDate = convertStringDate(fromDateS)
+        val toDate = convertStringDate(toDateS)
+
+        filteredCategories = unfilteredCategories.filter { category ->
+            val expenseDate : Date = convertStringDate(category.dateCreated)
+            expenseDate.after(fromDate) && expenseDate.before(toDate)
+
+        }
+
+        categoryAdapter.updateData(filteredCategories)
+    }
+
+    private fun convertStringDate(dateString: String): Date {
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", java.util.Locale.getDefault())
+        val fromDate: Date = try {
+            dateFormat.parse(dateString)
+        } catch (e: Exception) {
+            Toast.makeText(this, "Invalid From Date format", Toast.LENGTH_SHORT).show()
+            return Date()
+        }
+        return fromDate
+    }
 
     private suspend fun loadCategories() {
         firestore.collection("Categories").get()
@@ -106,6 +183,7 @@ class CategoryView : AppCompatActivity() {
                         amountBudgeted = doc.getLong("amountBudgeted")?.toDouble() ?: 0.0
                     )
                 }
+                unfilteredCategories = categories
                 categoryAdapter.updateData(categories)
             }
             .addOnFailureListener {
